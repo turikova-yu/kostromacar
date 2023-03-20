2fa14527cb322452615ca513bd658ea5053ca623
Julia T <tundrik_2210@mail.ru>
2023-03-20 21:55:24 +0300

    Change config/db.php

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=os.kostromacar.local;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=yii2basic',
+    'username' => 'admincontry',
+    'password' => '%PassCount!!2',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
